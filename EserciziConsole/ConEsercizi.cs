356c1ff047f2ea0474589c4c7e66f1b3cb71b132
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-20 09:43:27 +0100

    Add project files.

---
+++ EserciziConsole/ConEsercizi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EserciziLibrary;
+
+namespace EserciziConsole
+{
+    class ConEsercizi
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            long fattoriale = 0; 
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            double d = 0;
+            double inverso = 0;
+            int max = 0;
+            int min = 0;
+            Console.WriteLine("Inserisci un numero per calcolare il fattoriale");
+            n = int.Parse(Console.ReadLine());
+            fattoriale = Esercizi.CalcolaFattoriale(n);
+
+
+            Console.WriteLine("Inserisci un numero per calcolare il massimo");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            max = Esercizi.CalcolaMax(a,b,c);
+
+
+            Console.WriteLine("Inserisci un numero per calcolare il Minimo");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            min = Esercizi.CalcolaMin(a,b,c);
+
+
+            Console.WriteLine("Inserisci un numero per calcolare l'Inverso");
+            d = double.Parse(Console.ReadLine());
+            inverso = Esercizi.CalcolInverso(d);
+        }
+    }
+}
---
+++ EserciziLibrary/Esercizi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EserciziLibrary
+{
+    public class Esercizi
+    {
+        public static long CalcolaFattoriale(long n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * CalcolaFattoriale(n - 1);
+        }
+        public static int CalcolaMax(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > c && b > a)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+         public static int CalcolaMin(int a,int b,int c)
+         {
+            if (a < b && a < c)
+            {
+                return a;
+            }
+            else if (b < c && b < a)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+         }
+        public static double CalcolInverso(double d)
+        {
+            return 1 / d;
+        }
+    }
+}
